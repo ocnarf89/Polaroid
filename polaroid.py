00d59cbc87196250429403f533118510b30c43b8
Franco David Miguez <franco.miguez98@gmail.com>
2020-07-29 15:23:42 -0300

    se agrego comentarios a las funciones y se acomodo un poco para que sea mas lejible y ordenado ademas se aclaro la vecion

--- polaroid.py
+++ polaroid.py
@@ -1,31 +1,34 @@
+#!/usr/bin/env python3
 #1157px = 10 cm con una resolucion de 300 ppp
 from PIL import Image
-dies_cm = 1157 
-imagen = Image.open("1.png")
+dies_cm = 1157
+imagen = Image.open("0.png")
 imagen_nueva = None
-ancho, alto = imagen.size
 
-def proporcion(tamaño_final,tamaño_cambiar,tamaño_scalable):
+#toma:
+#tamaño final(el tamaño que queremos como resultado)
+#tamaño cambiar (el tamaño que nos interesa que se modifique a tal tamaño)
+#tamaño scalable (tamaño que nos interesa que se modifique proporcional mente)
+def proporcion (tamaño_final,tamaño_cambiar,tamaño_scalable):
    return  int((tamaño_scalable * tamaño_final) / tamaño_cambiar)
-   #return imagen.resize((dies_cm,int(proporcional)))
 
-def redimencionado(image,):
-   if ancho > alto:
-      imagen_nueva = imagen.resize(
-					(dies_cm,
-					proporcion(dies_cm,ancho,alto))
+#toma la imagen a modificar y el tamaño deciado
+#y lo redimenciona y guarda
+def redimencionado(image,tamaño):
+    ancho, alto = imagen.size
+    if ancho > alto:
+        imagen_nueva = imagen.resize(
+					(tamaño,
+					proporcion(tamaño,ancho,alto))
 					)
-      imagen_nueva.save("redimencionado.png","png")
+        imagen_nueva.save("redimencionado.png","png")
 
-   else:
-      imagen_nueva = imagen.resize(
-					(proporcion(dies_cm,alto,ancho),
-					dies_cm)
+    else:
+        imagen_nueva = imagen.resize(
+					(proporcion(tamaño,alto,ancho),
+					tamaño)
 					)
-      imagen_nueva.save("redimencionado.png","png")
-
-   
+        imagen_nueva.save("redimencionado.png","png")
 
 
-  
-
+redimencionado(imagen,dies_cm)
